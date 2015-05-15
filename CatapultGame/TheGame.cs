17ad80c4c31debe50f6b872de4d654c8866f0940
Авраменко Виталий <kiuby@live.ru>
2015-05-15 15:07:45 +0300

    a

--- CatapultGame/TheGame.cs
+++ CatapultGame/TheGame.cs
@@ -46,7 +46,7 @@ namespace GoblinsGame
             Components.Add(screenManager);
 
             // TODO: Add gameplay screen
-            screenManager.AddScreen(new GameplayScreenMap(false), null);
+            //screenManager.AddScreen(new GameplayScreenMap(false), null);
 
             // TODO: Add additional screens
             // TODO: Add audio manager
