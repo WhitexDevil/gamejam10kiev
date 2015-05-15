f0f65d0a5dd96ed407d624c8127583086b1a0268
Maxim <maxsmelacom29@gmail.com>
2015-05-15 13:55:02 +0300

    Create class Map

---
+++ CatapultGame/Screens/Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatapultGame.Screens
+{
+    public class Map
+    {
+        List<List<Ground>> map;
+
+
+
+    }
+}
