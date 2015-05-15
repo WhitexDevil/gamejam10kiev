f9b0b27c61f0bfefd3afe51249bd448f2d42afda
Авраменко Виталий <kiuby@live.ru>
2015-05-15 19:06:14 +0300

    some Updates

--- CatapultGame/BattleComponent/Squad.cs
+++ CatapultGame/BattleComponent/Squad.cs
@@ -10,10 +10,14 @@ namespace CatapultGame
 {
     public class Squad 
     {
+        float TimeUpdate = 0.0001F;
+        float TimeDraw = 0.0001F;
         public readonly Unit Unit;
         private int amount;
         public int DamageLeft;
-        private Point position;
+        public Point position;
+        public int start;
+        Rectangle CurrentFrame;
 
         public enum ActionType { None, Move, Attack, MoveAndAttack, AttackAndMove, TakingDamage }
 
@@ -37,29 +41,78 @@ namespace CatapultGame
 
         public void Draw(GameTime gameTime)
         {
-            switch (CurrentAction.Type)
-            {
-                case ActionType.None:
-                    break;
-                case ActionType.Move:
-                    break;
-                case ActionType.Attack:
-                    break;
-                case ActionType.MoveAndAttack:
-                    break;
-                case ActionType.AttackAndMove:
-                    break;
-                case ActionType.TakingDamage:
-                    break;
-                default:
-                    break;
+
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (elapsed >= TimeDraw)
+            { 
+                switch (CurrentAction.Type)
+                {
+                    case ActionType.None:
+                        break;
+                    case ActionType.Move:
+                        break;
+                    case ActionType.Attack:
+
+                        CurrentAction = new Action() { Type = ActionType.None };
+                        break;
+                    case ActionType.MoveAndAttack:
+                        break;
+                    case ActionType.AttackAndMove:
+                        break;
+                    case ActionType.TakingDamage:
+
+
+                        CurrentAction = new Action() { Type = ActionType.None };
+
+                        break;
+                    default:
+                        break;
+                }
+
             }
-
-
         }
         public void Update(GameTime gameTime)
         {
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (elapsed >= TimeUpdate)
+            {
+                switch (CurrentAction.Type)
+                {
+                    case ActionType.None:
+                        break;
+                    case ActionType.Move:
+                        if(start == 999)
+                        {
+                            start =CurrentAction.Path.Length-1;
+                        }
+                        Point b = CurrentAction.Path[start - 1];
+                        Point a = position;//CurrentAction.Path[start];
+                        Vector2 delta = new Vector2(b.X - a.X, b.Y - a.Y);
+                        Vector2.Normalize(delta);
+                        position = new Point(position.X + (int)delta.X, position.Y + (int)delta.Y);
+                        if (position==CurrentAction.Path[start-1])
+                        {
+                            start--;
+                        }
+                        if (start <= 0) ;
+                        CurrentAction = new Action() { Type = ActionType.None };
+                        break;
+                    case ActionType.Attack:
+                        if (CurrentAction.Target.CurrentAction.Type!=ActionType.TakingDamage)
+                        Attack(CurrentAction.Target);
+                        
+                        break;
+                    case ActionType.MoveAndAttack:
 
+                        break;
+                    case ActionType.AttackAndMove:
+                        break;
+                    case ActionType.TakingDamage:
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
 
 
@@ -93,6 +146,7 @@ namespace CatapultGame
             amount = unit.MaxAmount;
             DamageLeft = 0;
             Position = new Point();
+            start = 999;
             alive = true;
         }
 
@@ -108,11 +162,12 @@ namespace CatapultGame
             target.TakeDamage(Damage);
         }
 
-        public void TakeDamage(int Damage)
+        public void TakeDamage(int damage)
         {
-            Damage += DamageLeft;
-            Amount -= (Damage / Unit.MaxHitpoints);
-            DamageLeft = Damage % Unit.MaxHitpoints;
+            CurrentAction = new Action() {Type = ActionType.TakingDamage,Damage =damage  };
+            damage += DamageLeft;
+            Amount -= (damage / Unit.MaxHitpoints);
+            DamageLeft = damage % Unit.MaxHitpoints;
 
 
         }
